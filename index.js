db70724f83a4c7319ccd3d4afee205cac34c03b6
Asma Sadia <a.sadia51299@gmail.com>
2017-10-07 03:13:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const app = "I don't do much."
 var kittens= ["Milo", "Otis", "Garfield"]
-
+//directly add a kitten at the END of the array
 function destructivelyAppendKitten(name){
   kittens.push(name)
   return kittens
+}
+//add a kitten at the BEGN. of the array
+function destructivelyPrependKitten(name){
+  kiitens.unshift(name)
+  return kittens
+  
 }
